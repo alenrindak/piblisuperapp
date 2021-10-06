7e788bb24a954c365c4fcf163134de5d508c7933
alenrindak <55909964+alenrindak@users.noreply.github.com>
2021-10-06 10:41:05 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar HTML Tag Iframe Pada HTML | www.malasngoding.com</title>
+</head>
+<body>
+<a href="http://www.pibli.epizy.com/"><button>Button Dengan Link</button></a>
+</body>
+</html>
